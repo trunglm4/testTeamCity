0ad3e175aa12efcc89d23b86b317ea298d39a38a
Minh Trung <leemin@LeeMins-MacBook-Pro.local>
2018-07-24 15:07:33 +0700

    init

---
+++ src/main/java/Math.java
@@ -0,0 +1,10 @@
+public class Math {
+    int a, b;
+    Math(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int add() {
+        return a + b;
+    }
+}
---
+++ src/test/java/MathTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class MathTest {
+    Math math;
+    @Before
+    public void setUp() throws Exception {
+        math = new Math(7, 10);
+    }
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(17, math.add());
+    }
+}
